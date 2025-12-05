d2f5672e9d2d0e53ccc4276a03074a076e66c09a
juliomarttins <maretins10@gmail.com>
2025-12-05 13:14:29 -0300

    Update comments and target URL in get.ps1

--- get.ps1
+++ get.ps1
@@ -1,17 +1,17 @@
-
+# [INICIO_COMENTARIO] ----- ARQUIVO: get.ps1 (Launcher) ----- [FIM_COMENTARIO]
 $ErrorActionPreference = "Stop"
 
+# Link corrigido para a pasta 'Versão Tudo-em-Um-KL' (com codificação para acentos)
 $TargetUrl = "https://alfa-info.vercel.app/MAS/Vers%C3%A3o%20Tudo-em-Um-KL/MAS_AIO.cmd"
 
-
-
+# Local temporário para salvar
 $File = "$env:TEMP\MAS_AIO.cmd"
 
-
+# Baixa o ativador real
 Write-Host "Baixando Alfa Ativador..." -ForegroundColor Green
 Invoke-WebRequest -Uri $TargetUrl -OutFile $File
 
-
+# Executa
 Write-Host "Iniciando..." -ForegroundColor Green
 Start-Process $File -Wait
-
+# [INICIO_COMENTARIO] ----- FIM DO ARQUIVO ----- [FIM_COMENTARIO]
