3c42df0f7f1d1d6e66d465e44bdd7b1625898571
juliomarttins <maretins10@gmail.com>
2025-12-05 13:08:24 -0300

    Update target URL in get.ps1 script

--- get.ps1
+++ get.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 # 1. Defina a URL do seu arquivo principal (O CMD ou ZIP que você baixou)
 # Se estiver na raiz do repo, a URL será algo como:
-$TargetUrl = "https://alfa-info.vercel.app/MAS/All-In-One-Version/MAS_AIO.cmd" 
+$TargetUrl = "https://alfa-info.vercel.app/MAS/Vers%C3%A3o%20Tudo-em-Um-KL/MAS_AIO.cmd"
 # (VERIFIQUE O CAMINHO EXATO DA PASTA ONDE VOCÊ COLOCOU O ARQUIVO NO GITHUB)
 
 # 2. Cria um nome temporário
